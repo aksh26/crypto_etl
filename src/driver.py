79db074a774620ccb051ba6926e1557337361569
Akhil Sharma <akhilsharmajiit@gmail.com>
2024-03-13 08:50:04 +0530

    Update driver.py

--- src/driver.py
+++ src/driver.py
@@ -1,1 +1,54 @@
+from extract import Extractor
+from transform import Transformer
+from load import Loader
+from utils.config import DATABASE, SCHEMA, TABLE, LOG_FILE_LOC
 
+
+import logging
+
+logging.basicConfig(filename=LOG_FILE_LOC, level=logging.INFO)
+
+class ETLDriver:
+    def __init__(self):
+        pass
+
+    def run(self):
+        try:
+            logging.info("Starting ETL pipeline")
+
+            # Extraction
+            logging.info("Extracting data from BigQuery")
+            extractor = Extractor(DATABASE, SCHEMA, TABLE)
+            extract_data = extractor.extract()
+
+        except Exception as e:
+            logging.error(f"Error while doing extraction in ETL pipeline: {str(e)}")
+
+        try:
+            # Transformation
+            logging.info("Transforming data")
+            transformer = Transformer()
+            transformed_data = transformer.transform(extract_data)
+
+        except Exception as e:
+            logging.error(f"Error while transforming in ETL pipeline: {str(e)}")
+
+        try:
+            # Loading
+            logging.info("Loading data to Parquet")
+            loader = Loader()
+            loader.load(transformed_data)
+            logging.info("ETL pipeline completed successfully")
+
+        except Exception as e:
+            logging.error(f"Error while loading in ETL pipeline: {str(e)}")
+
+def main():
+    """Inside Main Method"""
+
+    # Run the ETL pipeline
+    etl_driver = ETLDriver()
+    etl_driver.run()
+
+if __name__ == "__main__":
+    main()
