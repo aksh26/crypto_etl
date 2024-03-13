a9f850f18f3c188ce3bb7f460922e7eb3d6651c3
Akhil Sharma <akhilsharmajiit@gmail.com>
2024-03-13 08:54:36 +0530

    Update config.py

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,1 +1,7 @@
+import os
 
+DATABASE = os.environ.get("DATABASE","bigquery-public-data")
+SCHEMA = os.environ.get("SCHEMA","crypto_zilliqa")
+TABLE = os.environ.get("TABLE","transactions")
+RESULT_FILE_LOC = os.environ.get("RESULT_FILE_LOC","output_files/result.parquet")
+LOG_FILE_LOC = os.environ.get("LOG_FILE_LOC","output_files/etl_pipeline.log")
